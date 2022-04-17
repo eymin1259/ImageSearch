61e397df86f85af20dd1486117ea4f93496eb8b6
eymin1259 <eymin1259@gmail.com>
2022-04-17 15:39:18 +0900

    ADD: searchBar, collectionview UI 개발

--- ImageSearch/ImageSearch/Source/ViewController/PhotoViewController.swift
+++ ImageSearch/ImageSearch/Source/ViewController/PhotoViewController.swift
@@ -7,12 +7,33 @@
 
 import UIKit
 import ReactorKit
+import SnapKit
 
-class PhotoViewController : UIViewController, View {
+final class PhotoViewController : UIViewController, View {
     
+    // MARK: properties
     var disposeBag: DisposeBag = .init()
     typealias Reactor = PhotoReactor
     
+    // MARK: UI
+    private var photoSearchBar: UISearchBar = {
+        let searchbar = UISearchBar(frame: .zero)
+        searchbar.placeholder = "검색어를 입력하세요."
+        searchbar.searchBarStyle = .prominent
+        searchbar.translatesAutoresizingMaskIntoConstraints = false
+        return searchbar
+    }()
+    
+    private var photoCollectionView : UICollectionView = {
+        let layout: UICollectionViewFlowLayout = .init()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.bounces = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    //MARK: initialize
     init(reactor: Reactor) {
         super.init(nibName: nil, bundle: nil)
         self.reactor = reactor
@@ -22,18 +43,48 @@ class PhotoViewController : UIViewController, View {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
+    //MARK: life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .red
+        configureUI()
     }
 
+    //MARK: methods
+    
+    func configureUI(){
+        
+        view.backgroundColor = .white
+        [photoSearchBar, photoCollectionView].forEach {
+            view.addSubview($0)
+        }
+        
+        let guide = view.safeAreaLayoutGuide
+        photoSearchBar.snp.makeConstraints { make in
+            make.top.equalTo(guide)
+            make.leading.equalTo(guide)
+            make.trailing.equalTo(guide)
+        }
+        
+        let layout: UICollectionViewFlowLayout = .init()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        let width = (view.frame.width ) / 3
+        layout.itemSize = CGSize(width: width, height: width)
+        photoCollectionView.collectionViewLayout = layout
+        
+        photoCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(photoSearchBar.snp.bottom)
+            make.leading.equalTo(guide)
+            make.trailing.equalTo(guide)
+            make.bottom.equalTo(guide)
+        }
+    }
     
     func bind(reactor: PhotoReactor) {
-        print("Debug : vc bind")
+        //action
+        
+        //state
     }
-    
 
 }
-
