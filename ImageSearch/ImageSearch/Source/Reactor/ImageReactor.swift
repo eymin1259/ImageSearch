ab1a8ba3d24c45b902381f65f42e6b9ab25d4b30
eymin1259 <eymin1259@gmail.com>
2022-04-17 18:31:04 +0900

    ADD: action, mutation, state, mutate(), reduce() 정의 및 mock데이터 연결

--- ImageSearch/ImageSearch/Source/Reactor/ImageReactor.swift
+++ ImageSearch/ImageSearch/Source/Reactor/ImageReactor.swift
@@ -10,38 +10,57 @@ import ReactorKit
 
 final class ImageReactor : Reactor {
 
+    //MARK: properties
     enum Action {
-        
+        case inputQuery(String)
     }
     
     enum Mutation {
-        
+        case searchImages([Image])
     }
     
     struct State {
-        
+        var imageList = [Image]()
     }
     
     let initialState: State
     var imageRepository : ImageRepositoryType
     
-    // MARK: Initializers
-    
+    // MARK: initialize
     init(imageRepository : ImageRepositoryType) {
-        self.initialState = State()
+        self.initialState  = .init()
         self.imageRepository = imageRepository
     }
 }
 
 extension ImageReactor {
     
-    func mutate(action: Action) -> Observable<Mutation> {
-        
+    //MARK: Mutate
+    func mutate(action: ImageReactor.Action) -> Observable<ImageReactor.Mutation> {
+        switch action {
+        case .inputQuery(let query):
+            let path = Bundle.main.path(forResource: "Mock", ofType: "json") ?? ""
+            let jsonString = try? String(contentsOfFile: path) ?? ""
+            let decoder = JSONDecoder()
+            let data = jsonString!.data(using: .utf8)
+            if let data = data,
+               let res = try? decoder.decode(Response<Image>.self, from: data) {
+                return Observable.just(Mutation.searchImages(res.documents))
+            }
+            else{
+                return Observable.just(Mutation.searchImages([]))
+            }
+     
+        }
     }
     
-    func reduce(state: State, mutation: Mutation) -> State {
+    //MARK: Reduce
+    func reduce(state: State, mutation: ImageReactor.Mutation) -> State {
         var newState = state
-
+        switch mutation {
+        case .searchImages(let imageList):
+            newState.imageList = imageList
+        }
         return newState
     }
 }
