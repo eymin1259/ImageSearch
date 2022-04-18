fbacdf9d07b07031be68ce79f3ecdb6ac771debd
eymin1259 <eymin1259@gmail.com>
2022-04-18 22:26:16 +0900

    ADD: ImageCell 정의

--- ImageSearch/ImageSearch/Source/View/ImageCell.swift
+++ ImageSearch/ImageSearch/Source/View/ImageCell.swift
@@ -9,4 +9,16 @@ import UIKit
 
 final class ImageCell : UICollectionViewCell {
     
+    //MARK: properties
+    static let id = "ImageCellID"
+    
+    // MARK: initialize
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
