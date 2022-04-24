50e3f8f2c42b53ec93325075b9ff836dcd6a8013
eymin1259 <eymin1259@gmail.com>
2022-04-24 19:17:50 +0900

    ADD : Moya Provider생성시 NetworkLogger 플러그인 추가

--- ImageSearch/ImageSearch/Source/Data/Network/NetworkService.swift
+++ ImageSearch/ImageSearch/Source/Data/Network/NetworkService.swift
@@ -12,7 +12,7 @@ import RxMoya
 
 protocol NetworkServiceType {
     var provider:  MoyaProvider<MultiTarget> { get }
-    func request<API>(api: API) -> Observable<Response<API.Documents>>  where API : BaseServiceAPI
+    func request<API>(api: API) -> Observable<SearchResponse<API.Documents>>  where API : BaseServiceAPI
 }
 
 final class NetworkService : NetworkServiceType {
@@ -22,14 +22,26 @@ final class NetworkService : NetworkServiceType {
     var provider : MoyaProvider<MultiTarget>
     
     init() {
-        self.provider = MoyaProvider<MultiTarget>()
+        self.provider = NetworkService.makeProvider()
     }
     
-    func request<API>(api: API) -> Observable<Response<API.Documents>>  where API : BaseServiceAPI {
+    static func makeProvider() -> MoyaProvider<MultiTarget> {
+        // Set network logger plugin
+        let loggerPlugin = NetworkLoggerPlugin()
+        return MoyaProvider<MultiTarget>(plugins: [loggerPlugin])
+    }
+
+    func request<API>(api: API) -> Observable<SearchResponse<API.Documents>>  where API : BaseServiceAPI {
+        
         let endpoint = MultiTarget.target(api)
         return self.provider.rx.request(endpoint)
-            .filterSuccessfulStatusCodes()
             .asObservable()
-            .map { try JSONDecoder().decode(Response<API.Documents>.self, from: $0.data) }
+            .map {
+                try JSONDecoder().decode(SearchResponse<API.Documents>.self, from: $0.data)
+            }
+            .catch { err in
+                return .empty()
+            }
+            
     }
 }
